200c8eadec18bc65c5e2f6ab1d65640ec105008f
methumk <methum.cg@gmail.com>
2021-10-04 14:42:01 -0700

    testing

--- main.cpp
+++ main.cpp
@@ -1,2 +1,26 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+
+
+int main(){
+    int win_width = 1200;
+    int win_height = 750;
+    sf::RenderWindow window(sf::VideoMode(win_width, win_height), "Dijkstra", sf::Style::Close | sf::Style::Resize);
+
+    sf::CircleShape check(15);
+    check.setFillColor(sf::Color::Red);
+    while(window.isOpen()){
+        sf::Event event; 
+            while(window.pollEvent(event)){
+                switch (event.type){
+                    case sf::Event::Closed:
+                        window.close();
+                        break;
+                }
+            }
+
+        window.clear();
+        window.draw(check);
+        window.display();
+    }
+}
