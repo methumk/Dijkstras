97e648ad4b1787dc6c3eb94146ba7f071a6090b9
methumk <methum.cg@gmail.com>
2021-10-08 23:52:46 -0700

    Can create and join node through graph interface; problems with graph vector in terms of deleting a vector from all_Graphs when joining

--- main.cpp
+++ main.cpp
@@ -30,32 +30,109 @@ int main(){
         window.display();
     } */
 
-    /* Graph mygraph;
-    Node* hold;
-    Node s1(mygraph.getCurrNodeIdent());
-
-    s1.addNode(new Node(mygraph.getCurrNodeIdent()), 10, mygraph.getCurrLinkIdent());
-    s1.addNode(new Node(mygraph.getCurrNodeIdent()), 12, mygraph.getCurrLinkIdent()); 
-        hold = s1.getAdjNode((unsigned int)0);
-        hold->addNode(s1.getNodeInfo((unsigned int)1));
-
+    Graph mygraph;
+    /*
+        0->(1, 2, 3)
+        1->(*0, 2, 4)
+        2->(*0)
+        3->(*0, 4)
+        4->(*1, *3)
+    */
+    /* Node* hold, *t2;
+    Node s1(mygraph.getNewNodeIdent());
+    s1.addNode(new Node(mygraph.getNewNodeIdent()), 10, mygraph.getNewLinkIdent());
+    s1.addNode(new Node(mygraph.getNewNodeIdent()), 12, mygraph.getNewLinkIdent()); 
+    s1.addNode(new Node(mygraph.getNewNodeIdent()), 26, mygraph.getNewLinkIdent());
+        //Node 1 -> (2, 4)
+        hold = s1.getAdjNode(((unsigned int)0));
+        hold->addNode(new Node(mygraph.getNewNodeIdent()), 3, mygraph.getNewLinkIdent());
+        hold->addNode(s1.getNodeInfo(((unsigned int)1)));
+    
+        //Node 3 -> (4)
+        t2 = s1.getAdjNode(((unsigned int)2));
+        t2->addNode(hold->getNodeInfo(((unsigned int)0)));
     s1.printCurrNode();
     s1.printAdjNodes();
-    std::cout << "\n\t";
-    hold->printCurrNode();
-    hold->printAdjNodes();
-
-    Node s2(mygraph.getCurrNodeIdent());
-    s2.addNode(new Node(mygraph.getCurrNodeIdent()), 2, mygraph.getCurrLinkIdent());
-    s2.addNode(new Node(mygraph.getCurrNodeIdent()), 4, mygraph.getCurrLinkIdent());
-    s2.addNode(new Node(mygraph.getCurrNodeIdent()), 8, mygraph.getCurrLinkIdent());
-
-    s2.printCurrNode();
-    s2.printAdjNodes();
-
+    
     mygraph.addGraph(&s1);
-    mygraph.addGraph(&s2);  */
 
+
+    std::cout << "\n\n";
+    hold = mygraph.findNode(0, 99);
+
+    if (hold == NULL) std::cout << "hold NULL\n";
+    if (4 == hold->getNodeIdent()){
+        std::cout << "found correct Node " << hold->getNodeIdent() << "\n";
+    }else{
+        std::cout << "INCORRECT NODE " << hold->getNodeIdent() << "\n";
+    } */
+    
+    //Test Cases that work:
+    /* //(0, 1)
+    mygraph.createNewNode(mygraph.getNewNodeIdent());
+    mygraph.createNewNode(mygraph.getNewNodeIdent());
+        std::cout<<"All graphs size: " << mygraph.all_graphs_size() <<'\n';
+    //(0-1)
+    mygraph.joinNodes(0, 1, 20);
+        std::cout<<"All graphs size: " << mygraph.all_graphs_size() <<'\n';
+    //(2)
+    mygraph.createNewNode(mygraph.getNewNodeIdent());
+        std::cout<<"All graphs size: " << mygraph.all_graphs_size() <<'\n';
+    //(0-(1, 2))
+    mygraph.joinNodes(0, 2, 30);  
+    //(3)
+    mygraph.createNewNode(mygraph.getNewNodeIdent());
+        std::cout<<"All graphs size: " << mygraph.all_graphs_size() <<'\n';
+    //(0-(1, 2, 3))
+    mygraph.joinNodes(0, 3, 40);
+
+    //(4, 5)
+    mygraph.createNewNode(mygraph.getNewNodeIdent());
+    mygraph.createNewNode(mygraph.getNewNodeIdent());
+    std::cout<<"All graphs size: " << mygraph.all_graphs_size() <<'\n';
+    //(4-5)
+    mygraph.joinNodes(4, 5, 50);
+    std::cout<<"All graphs size: " << mygraph.all_graphs_size() <<'\n';
+
+    //(0-(1, 2, 3, 5-(4))))
+    mygraph.joinNodes(0, 5, 40);
+    std::cout<<"All graphs size: " << mygraph.all_graphs_size() <<'\n';
+
+    //(6)
+    mygraph.createNewNode(mygraph.getNewNodeIdent());
+    mygraph.joinNodes(3, 6, 23); */
+
+    //Think problem lies in vector, you join two nodes and the other vector nodes in the node_loc is messed up
+    //probably from vector.erase, probably could just make pointer null but that would waste space
+        //otherwise would have to move all the other graphs down - DFS to move all of them
+        //this means all_graphs size is not reflective of how many graphs there are
+    //(0, 1, 2, 3, 4)
+    /* mygraph.createNewNode();
+    mygraph.createNewNode();
+    mygraph.createNewNode();
+    mygraph.createNewNode();
+    mygraph.createNewNode();
+        std::cout<<"All graphs size: " << mygraph.getNumGraphs() <<'\n';
+    //(0-1)
+    mygraph.joinNodes(0, 1, 12);
+        std::cout<<"All graphs size: " << mygraph.getNumGraphs() <<'\n';
+    //(4-3)
+    mygraph.joinNodes(4, 3, 12);
+        std::cout<<"All graphs size: " << mygraph.getNumGraphs() <<'\n';
+    //(0-1-[4-[3]])
+    mygraph.joinNodes(1, 4, 12);
+        std::cout<<"All graphs size: " << mygraph.getNumGraphs() <<'\n';
+    //(0-1-[4-[3], 3-[4]])
+    mygraph.joinNodes(1, 3, 12);
+        std::cout<<"All graphs size: " << mygraph.getNumGraphs() <<'\n';
+    mygraph.joinNodes(3, 2, 12);
+
+    std::cout << "\n\n";
+    Node* fn = mygraph.REVISEDfindNode(2);
+    if (fn){
+        std::cout << "Found Node: " << fn->getNodeIdent() <<'\n'; 
+    }*/
+       
 
     return 0;
 }
--- structure.hpp
+++ structure.hpp
@@ -2,29 +2,38 @@
 structure.hpp
     - 
 */
+#pragma once
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include <cstdlib>
 #include <vector>
 #include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+
+//Define ull for identifiers
+typedef unsigned long long ull;
 
 
 /*
 Node class:
     - Node identifier
     - Circle shape??
-    - links (tuple: pointed node, link weight, link identifier)
+    - links (tuple: curr node, link weight, link identifier)
 */
 class Node{
+public:
+    typedef std::tuple<Node*, unsigned int, ull> ADJ_NODE;
+    typedef std::vector<ADJ_NODE> NODE_VEC;
 private:
-    typedef std::tuple<Node*, unsigned int, double> ADJ_NODE;
-    typedef std::vector<ADJ_NODE> NODE_LIST;
-
-    double ident;
-    NODE_LIST links;
+    ull ident;
+    std::vector<ADJ_NODE> links;
     sf::CircleShape GUInode;
 public:
     Node(){}
-    Node(double i): ident(i){}
+    Node(ull i): ident(i){}
 
+    //creates the node for node visualization in the GUI
     void addNodeShape(float radius, sf::Vector2f& pos, sf::Color color){
         GUInode.setRadius(radius);
         GUInode.setOrigin(radius, radius);
@@ -34,11 +43,12 @@ public:
         GUInode.setOutlineThickness(2.f);
     }
 
-    //add a new linked up node
-    void addNode(Node* addNode, unsigned int lw, double li){
+    //add a new linked up node manually
+    void addNode(Node* addNode, unsigned int lw, ull li){
         links.push_back(std::make_tuple(addNode, lw, li));
     }
 
+    //add a new linked up node by tuple
     void addNode(ADJ_NODE addNode){
         links.push_back(addNode);
     }
@@ -47,6 +57,7 @@ public:
         std::cout << "Node ident: " << ident << '\n';
     }
 
+    //print all information about nodes connected to current node
     void printAdjNodes(){
         for (unsigned int i = 0; i < links.size(); ++i){
             Node* hold = std::get<0>(links[i]);
@@ -58,22 +69,35 @@ public:
         }
     }
 
+    //returns current nodes identifier
+    inline ull getNodeIdent(){
+        return ident;
+    }
 
+    //returns vector of all nodes connected to current node
+    inline std::vector<ADJ_NODE>& getNodeLinks(){
+        return links;
+    }
+
+    //retrieves node and link information at the given links index
     ADJ_NODE getNodeInfo(unsigned int i){
         return links[i];
     }
 
     //have to do dfs on graph to find correct identifier
-    ADJ_NODE getNodeInfo(double i){
+    //retrieves node and link information at the given node identifier
+    ADJ_NODE getNodeInfo(ull i){
         return links[i];
     }
 
+    //retrieves Node for a node at the given links index
     Node* getAdjNode(unsigned int i){
         return std::get<0>(links[i]);
     }
 
     //have to do dfs on graph to find correct identifier
-    Node* getAdjNode(double ident){
+    //retrieves Node for the node with the given node identifier
+    Node* getAdjNode(ull ident){
         return NULL;
     }
 };
@@ -84,25 +108,170 @@ Graph class:
     - vector of all UNJOINED graphs (holds the head of each graph)
     - if you want to iterate through an individual graph you need to check for already visited nodes (graph traversal)
     - Merging graphs and updating the all_graphs vector
+    - handle deletion of a Node (what happens to its links)
 */
 class Graph{
 private:
+    typedef std::tuple<Node*, unsigned int, ull> ADJ_NODE;
     std::vector<Node*> all_graphs;
-    double curr_node_ident;
-    double curr_link_ident;
-
+    size_t num_graphs;
+    ull curr_node_ident;
+    ull curr_link_ident;
+    //For node_locs: <Node identifier, all_graphs index>
+    std::unordered_map<ull, unsigned int> node_locs;
 public:
-    Graph(): curr_node_ident(0), curr_link_ident(0){};
+    Graph(): curr_node_ident(0), curr_link_ident(0), num_graphs(0){};
 
-    inline double getCurrNodeIdent(){
+    //returns a valid Node identifier and updates next available identifier
+    inline ull getNewNodeIdent(){
         return curr_node_ident++;
     }
 
-    inline double getCurrLinkIdent(){
+    //returns a valid Link identifier and updates next available identifier
+    inline ull getNewLinkIdent(){
         return curr_link_ident++;
     }
 
+    //Adds a Node to the all_graphs vector
     inline void addGraph(Node* n){
         all_graphs.push_back(n);
+    }
+
+    //returns size of the all_graphs vector
+    inline size_t all_graphs_size(){
+        return all_graphs.size();
+    }
+
+    //returns total number of existing graphs
+    inline size_t getNumGraphs(){
+        return num_graphs;
+    }
+
+    //Runs a depth first search on the graph to find node with the given identifier (Returns NULL if node was not in the graph)
+    Node* graphDFS(Node* curr, ull ident, std::unordered_set<ull>& visited){
+        std::cout << "Curr node: " << curr->getNodeIdent() << '\n';
+        if (curr->getNodeIdent() == ident){
+            return curr;
+        }else{
+            //mark current node as visited
+            visited.insert(curr->getNodeIdent());
+            //get the current nodes links
+            std::vector<ADJ_NODE> links = curr->getNodeLinks();
+
+            //search for the next unvisited node in the current nodes links vector
+            for (unsigned int i=0; i < links.size(); ++i){
+                Node* inspect = std::get<0>(links[i]);
+                Node* retval = NULL;
+                
+                std::cout << "\tCN: " << curr->getNodeIdent() << " searching link: " << inspect->getNodeIdent() << '\n';
+                if (!visited.count(inspect->getNodeIdent())){
+                    std::cout << "\tCN: " << curr->getNodeIdent() << " inspecting link: " << inspect->getNodeIdent() << '\n';
+                    retval = graphDFS(inspect, ident, visited);
+                    if (retval) return retval;
+                }
+
+                std::cout << "\tCN: " << curr->getNodeIdent() << " leaving link: " << inspect->getNodeIdent() << '\n';
+            }
+        }
+
+        //Couldn't find node with identity so return null adj_node
+        return NULL;
+    }
+
+    //finds a given node in one of the graphs and the node identifier 
+    //should only take ident as parameter
+    Node* findNode(unsigned int graph_index, ull ident){
+        std::unordered_set<ull> visited;
+        //write a map that determines which index in all_graphs the indentifier belongs to
+        return graphDFS(all_graphs[graph_index], ident, visited);
+    }
+
+
+    //runs DFS to find node with given identifier
+    //REVISED: you don't have to tell which all_graphs index to look into
+    Node* REVISEDfindNode(ull ident){
+        if (!node_locs.count(ident)){
+            std::cout << "ERROR in graph.REVISEDfindNode - Node location not recorded in map\n";
+            exit(EXIT_FAILURE);
+        }
+        std::unordered_set<ull> visited;
+        return graphDFS(all_graphs[node_locs[ident]], ident, visited);
+    }
+
+    //should take in a given point, and create a new node as its own graph
+    //records the new node into the map and stores which index in the graphs vector it is in
+    //right now just going to give it node identifier parameter
+    void createNewNode(){
+        ull ident = getNewNodeIdent();
+        all_graphs.push_back(new Node(ident));
+        node_locs.emplace(ident, all_graphs.size()-1);
+        num_graphs++;
+    }
+
+    //runs DFS and changes each identifier's to the new_loc in all_graphs
+    //Arguments(new all_graphs location, graph head to move, set to keep track of visited node)
+    void moveGraphLoc(unsigned int new_loc, Node* curr, std::unordered_set<ull>& visited){
+        //mark current node as visited
+        visited.insert(curr->getNodeIdent());
+        //move current node to the new all_graph locs
+        node_locs[curr->getNodeIdent()] = new_loc;
+        //get current node's links list
+        std::vector<ADJ_NODE> links = curr->getNodeLinks();
+
+
+        //check each node in currents links 
+        for (unsigned int i = 0; i < links.size(); ++i){
+            Node* inspect = std::get<0>(links[i]);
+            if (!visited.count(inspect->getNodeIdent())){
+                moveGraphLoc(new_loc, inspect, visited);
+            }
+        }
+    }
+
+    //should take in the points of two existing nodes and connect them together in their links vector
+    //check if two nodes are in the same graph
+        //if they are then just run DFS and connect the two
+        //else connect 1 to 2 and 2 to 1 and remove node 2 index from the graph index (update node locs as well)
+            //note if you connect any node from an index in all_graphs with another one, it becomes one graph so you can get rid of the entire pointer on one side
+            //have to change node locs of the graph you are moving though
+    //right now just going to give it node identifier parameter
+    void joinNodes(ull ident1, ull ident2, unsigned int link_weight){
+        if (!node_locs.count(ident1) || !node_locs.count(ident2)){
+            std::cout << "ERROR in graph.joinNodes: One or both of nodes to join have not been created before EXITING\n";
+            exit(EXIT_FAILURE);
+        }else{
+            unsigned int loc1 = node_locs[ident1], loc2 = node_locs[ident2];
+            Node* n1 = REVISEDfindNode(ident1);
+            Node* n2 = REVISEDfindNode(ident2);
+            if (!n1){
+                std::cout << "Join Nodes - N1(" << ident1 << ") WASNT FOUND but it should EXITING\n";
+                exit(EXIT_FAILURE);
+            }
+            if (!n2){
+                std::cout << "JOIN NODES - N2(" << ident2 << ") WASNT FOUND but it should EXITING\n";
+                exit(EXIT_FAILURE);
+            }
+
+            //update node 2's node_loc to be the same as node 1
+            if (loc1 != loc2){
+                std::unordered_set<ull> visited;
+                //update node_locs map for the graph thats getting moved
+                moveGraphLoc(loc1, all_graphs[loc2], visited);
+                //decrement total number of graphs
+                num_graphs--;
+            }
+
+            //CHECK TO SEE IF NODES ALREADY CONNECTED
+            //connect the two nodes together
+            ull link_ident = getNewLinkIdent();
+            n1->addNode(n2, link_weight, link_ident);
+            n2->addNode(n1, link_weight, link_ident);
+
+            //remove graph getting moved from all_graphs if locations are different
+            // if (loc1 != loc2) all_graphs.erase(all_graphs.begin()+loc2);
+            if (loc1 != loc2) all_graphs[loc2] = NULL;
+            
+        }
+
     }
 };
