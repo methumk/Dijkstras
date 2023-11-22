b4336d460ace2d2910bba15b1dca484b7067cf95
methumk <methum.cg@gmail.com>
2023-11-21 22:34:33 -0800

    remove weight map

--- algo.hpp
+++ algo.hpp
@@ -269,36 +269,4 @@ class Algos{
 
             runAlgo = NoAlgo;
         }
-
-       
-        void nodesToWM(Node* curr, std::unordered_set<Node*>& visited, weight_map& wm){
-            //mark current node as visited and save it in weight map
-            visited.insert(curr);
-            wm[curr->getNodeIdent()] = std::make_tuple(-1, ULLONG_MAX);
-
-            std::vector<ADJ_NODE> links = curr->getNodeLinks();
-            //add nodes to the weight map if they aren't already visited
-            for(size_t i=0; i < links.size(); ++i){
-                Node* inspect = std::get<0>(links[i]);
-                if (!visited.count(inspect))
-                    nodesToWM(inspect, visited, wm);
-            }
-
-
-        }
-
-        //returns the weight map from the start node to all other nodes in the graph
-        weight_map runDijkstra(Node* start_node, Graph& graphs){
-            std::cout << "Running Dijkstra\n";
-        
-            //find graph location of node
-            size_t graph_loc = graphs.getNodeGraphsPos(start_node->getNodeIdent());
-            if (graph_loc == -1) perror("\t\tDijkstra  - graph pos not found - exiting");
-
-            weight_map wm;
-            std::unordered_set<Node*> setwm_visited;
-            //save all nodes to the weight map 
-            nodesToWM(graphs.getGraphHead(graph_loc), setwm_visited, wm);
-
-        }
 };
